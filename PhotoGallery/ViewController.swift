df089bec43563fe6795d4c548adff09539a8c812
motto3532 <gropmor74@gmail.com>
2023-06-08 13:11:42 +0900

    Add photoLibrary function

--- PhotoGallery/ViewController.swift
+++ PhotoGallery/ViewController.swift
@@ -17,10 +17,35 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var pictureImage: UIImageView!
     
     @IBAction func cameraButtonAction(_ sender: Any) {
-        let imagePickerController = UIImagePickerController()
-        imagePickerController.sourceType = .camera
-        imagePickerController.delegate = self
-        present(imagePickerController, animated: true)
+        let alertController = UIAlertController(title: "confirmation", message: "Please select.", preferredStyle: .actionSheet)
+        
+        //camera
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "camera", style: .default, handler: {(action) in
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .camera
+                imagePickerController.delegate = self
+                self.present(imagePickerController, animated: true)
+            })
+            alertController.addAction(cameraAction)
+        }
+        
+        //photoLibrary
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "photoLibrary", style: .default, handler: {(action) in
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .photoLibrary
+                imagePickerController.delegate = self
+                self.present(imagePickerController, animated: true)
+            })
+            alertController.addAction(photoLibraryAction)
+        }
+        
+        //cancel
+        let cancelAction = UIAlertAction(title: "cancel", style: .cancel)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
